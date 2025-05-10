6c20c7ea4217cb8ecb477737cdd633af2c64f11b
fucho777 <k.n.t.bouzu777@gmail.com>
2025-05-10 14:08:18 +0900

    Fix useLocation import

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { ChefHat, Clock, Sparkles, Save, Search, PlayCircle, ArrowRight, Check, AlertCircle } from 'lucide-react';
 import Button from '../components/ui/Button';
 
